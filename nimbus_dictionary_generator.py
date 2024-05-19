ab99fb99252621185ae2d83fba8db8e8084746a0
C4sp3r <55068123+C4sp3r2222@users.noreply.github.com>
2024-05-19 11:54:26 +0200

    Update nimbus_dictionary_generator.py

--- nimbus_dictionary_generator.py
+++ nimbus_dictionary_generator.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-
-# Nimbus Dictionary Generator 2024: By C4sp3r
-
 import os
 from colorama import init, Fore
 from itertools import permutations
@@ -30,12 +26,12 @@ def generate_new_dictionary(words, new_dictionary_name):
                     f_out.write(combination_str + '\n')  # Guarda las combinaciones de palabras
                     total_lines_new += 1
 
-        print(Fore.YELLOW + f"[+] El diccionario final tendrá un total de {total_lines_new} líneas." + Fore.RESET)
+        print(Fore.YELLOW + f"[i] El diccionario final tendrá un total de {total_lines_new} líneas." + Fore.RESET)
         decision = input(Fore.CYAN + "[+] ¿Desea continuar con la creación del diccionario? (Si/No): " + Fore.RESET).lower()
         if decision in ['si', 'sí', 's']:
             print(Fore.GREEN + "[+] Diccionario generado con éxito:", new_dictionary_name + Fore.RESET)
         else:
-            print(Fore.YELLOW + "[+] Saliendo..." + Fore.RESET)
+            print(Fore.YELLOW + "[i] Saliendo..." + Fore.RESET)
     except Exception as e:
         print(Fore.RED + "[!] ERROR:", e + Fore.RESET)
 
@@ -53,12 +49,12 @@ def generate_new_dictionary_from_file(dictionary_path, characters, new_dictionar
                     f_out.write(modified_line + '\n')  # Guarda las combinaciones de caracteres
                     total_lines_new += 1
 
-        print(Fore.YELLOW + f"[+] El diccionario final tendrá un total de {total_lines_new} líneas." + Fore.RESET)
+        print(Fore.YELLOW + f"[i] El diccionario final tendrá un total de {total_lines_new} líneas." + Fore.RESET)
         decision = input(Fore.CYAN + "[+] ¿Desea continuar con la creación del diccionario? (Si/No): " + Fore.RESET).lower()
         if decision in ['si', 'sí', 's']:
             print(Fore.GREEN + "[+] Diccionario generado con éxito:", new_dictionary_name + Fore.RESET)
         else:
-            print(Fore.YELLOW + "[+] Saliendo..." + Fore.RESET)
+            print(Fore.YELLOW + "[i] Saliendo..." + Fore.RESET)
     except Exception as e:
         print(Fore.RED + "[!] ERROR:", e + Fore.RESET)
 
@@ -103,7 +99,7 @@ def merge_dictionaries(dictionary1_path, dictionary2_path, new_dictionary_name):
             total_lines_dict2 = len(lines_dict2)
             total_lines_merged = total_lines_dict1 + total_lines_dict2
 
-            print(Fore.YELLOW + f"\n[i] Se van a unir los diccionarios:")
+            print(Fore.YELLOW + f"[i] Se van a unir los diccionarios:")
             print(f"   [+] {dictionary1_path} -> {total_lines_dict1} líneas")
             print(f"   [+] {dictionary2_path} -> {total_lines_dict2} líneas")
 
@@ -112,10 +108,10 @@ def merge_dictionaries(dictionary1_path, dictionary2_path, new_dictionary_name):
             for line in lines_dict2:
                 f_out.write(line)
 
-        print(Fore.YELLOW + f"\n[i] El diccionario final tendrá {total_lines_merged} líneas en total.")
-        decision = input(Fore.CYAN + "\n[+] ¿Desea continuar con la creación del diccionario? (Si/No): " + Fore.RESET).lower()
+        print(Fore.YELLOW + f"[i] El diccionario final tendrá {total_lines_merged} líneas en total.")
+        decision = input(Fore.CYAN + "[+] ¿Desea continuar con la creación del diccionario? (Si/No): " + Fore.RESET).lower()
         if decision in ['si', 'sí', 's']:
-            print(Fore.GREEN + "\n[+] Diccionario generado con éxito:", new_dictionary_name + Fore.RESET)
+            print(Fore.GREEN + "[+] Diccionario generado con éxito:", new_dictionary_name + Fore.RESET)
         else:
             print(Fore.YELLOW + "[+] Saliendo..." + Fore.RESET)
     except Exception as e:
@@ -123,21 +119,23 @@ def merge_dictionaries(dictionary1_path, dictionary2_path, new_dictionary_name):
 
 def get_new_dictionary_name():
     """Obtiene el nombre para el nuevo diccionario."""
-    name = input(Fore.CYAN + "[+] Nombre del nuevo diccionario (Por defecto: Nuevo_diccionario.txt):\n   Ej: Mi_Nuevo_diccionario.txt\n" + Fore.RESET)
+    name = input(Fore.CYAN + "[+] Nombre del nuevo diccionario (Por defecto: diccionarios_unidos.txt):\n   Ej: Nuevo_diccionario.txt\n" + Fore.RESET)
     if not name:
-        name = "Nuevo_diccionario.txt"
+        name = "diccionarios_unidos.txt"
     return name
 
 def menu():
-    print("\ \ \ \ \ \                    | \ | (_)_ __ ___ | |__  _   _ ___                                ") 
-    print(" \ \ \ \ \ \_____ _____ _____  |  \| | | '_ ` _ \| '_ \| | | / __|  _____ _____ _____ _____ _____") 
-    print(" / / / / / /_____|_____|_____| | |\  | | | | | | | |_) | |_| \__ \ |_____|_____|_____|_____|_____|")
-    print("/_/_/_/_/_/   _   _            |_| \_|_|_| |_| |_|_.__/_\__,_|___/                 _              ")
-    print("|  _ \(_) ___| |_(_) ___  _ __   __ _ _ __ _   _   / ___| ___ _ __   ___ _ __ __ _| |_ ___  _ __  ")
-    print("| | | | |/ __| __| |/ _ \| '_ \ / _` | '__| | | | | |  _ / _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__| ")
-    print("| |_| | | (__| |_| | (_) | | | | (_| | |  | |_| | | |_| |  __/ | | |  __/ | | (_| | || (_) | |    ")
-    print("|____/|_|\___|\__|_|\___/|_| |_|\__,_|_|   \__, |  \____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   \n\n")
-    print("By C4sp3r\n\n")  
+    print("  _   _ _           _") 
+    print(" | \ | (_)_ __ ___ | |__  _   _  __") 
+    print(" |  \| | | '_ ` _ \| '_ \| | | / __|") 
+    print(" | |\  | | | | | | | |_) | |_| \__ \ ")
+    print(" |_| \_|_|_| |_| |_|_.__/_\__,_|___/ ")
+    print(Fore.YELLOW + " _ _ _ _ _")  
+    print(Fore.YELLOW + "\ \ \ \ \ \ ") 
+    print(Fore.YELLOW + " \ \ \ \ \ \_____ _____ _____ _____ _____ _____") 
+    print(Fore.YELLOW + " / / / / / /_____|_____|_____|_____|_____|_____|")
+    print(Fore.YELLOW + "/_/_/_/_/_/")
+    print("\nDictionary Generator - By C4sp3r\n\n")  
     """Muestra el menú de opciones."""
     print(Fore.YELLOW + "[+] Elige entre las opciones:\n" + Fore.RESET)
     print("    [1] Crear un diccionario con palabras:")
@@ -167,13 +165,14 @@ def main():
             new_dictionary_name = input(Fore.CYAN + "[+] Nombre del nuevo diccionario (Por defecto: diccionarios_unidos.txt):\n   Ej: diccionarios_unidos.txt\n" + Fore.RESET)
             if not new_dictionary_name:
                 new_dictionary_name = "diccionarios_unidos.txt"
-            dictionary1_path = input(Fore.CYAN + "\n[+] Dime la ruta del primer diccionario a usar:\n" + Fore.RESET)
-            dictionary2_path = input(Fore.CYAN + "\n[+] Dime la ruta del segundo diccionario:\n" + Fore.RESET)
+            dictionary1_path = input(Fore.CYAN + "[+] Dime la ruta del primer diccionario a usar:\n" + Fore.RESET)
+            dictionary2_path = input(Fore.CYAN + "[+] Dime la ruta del segundo diccionario:\n" + Fore.RESET)
             merge_dictionaries(dictionary1_path, dictionary2_path, new_dictionary_name)
         else:
-            print(Fore.RED + "\n[!] ERROR: Opción no válida." + Fore.RESET)
+            print(Fore.RED + "[!] ERROR: Opción no válida." + Fore.RESET)
     except KeyboardInterrupt:
         print(Fore.YELLOW + "\n[+] Saliendo..." + Fore.RESET)
 
 if __name__ == "__main__":
     main()
+
